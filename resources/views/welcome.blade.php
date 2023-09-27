24248a2b05fd30aa5d288ca3efb5925d8ece574f
Faisal Yusuf Nur Setyo <yussupcazzanova97@gmail.com>
2023-09-27 14:51:04 +0700

    updated welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,32 +17,10 @@
 
 
 <div class="w-screen">
-    <div class="p-6 mx-auto max-w-7xl lg:p-8 ">
-        <div class="mt-16">
-            <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:gap-8">
+    <div class="p-6 flex justify-center items-center max-w-7xl lg:p-8 mx-auto">
 
+            <img src="{{ asset('storage/images/illu02.png') }}" alt="" >
 
-
-                <a href="/admin" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-indigo-500">
-                    <div>
-                        <div class="flex items-center justify-center w-16 h-16 rounded-full bg-indigo-50 dark:bg-indigo-900/20">
-                            <x-tabler-package class="text-indigo-400" />
-                        </div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Administration Panel</h2>
-
-                        <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
-                            Administration panel for managing entities.
-                        </p>
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center w-6 h-6 mx-6 shrink-0 stroke-indigo-500">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                    </svg>
-                </a>
-
-            </div>
-
-        </div>
 
     </div>
 </div>
